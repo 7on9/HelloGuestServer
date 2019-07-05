bdefd140e5e7d77106b36de09d713b38a46a7fdb
Ống Tre <tamdaulong207@gmail.com>
2019-07-05 15:42:14 +0700

    no message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let io = require("socket.io").listen(server),
   socket = require("./socket")(io);
 // io.origins('*'| ['http://localhost:3000']);
 io.origins(() => { })
-let port = process.env.PORT || port;
+let port = process.env.PORT || 1304;
 server.listen(port, 'http://3.17.161.232/', () => {
   console.log('Server listening on port ' + port);
 });
